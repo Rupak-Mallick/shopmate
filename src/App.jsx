36ea359700e69684d1911975974cc9e0032cc6ba
Rupak-Mallick <rupak.stat16@gmail.com>
2024-08-25 20:49:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+      <>
+          <div>
+            <Header/>
+            <AllRoutes/>
+          </div>
+      </>
+      )
+}
+
+export default App
